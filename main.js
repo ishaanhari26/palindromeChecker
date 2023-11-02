ba66e3d0906c714b8125250721cd3482a4e162bb
FayzaanV <fayzaanvirk6@yahoo.com>
2023-11-02 15:29:38 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let palindromeInput = document.querySelector("#palindromeInput");
+let submitButton = document.querySelector("#submitButton");
+let result = document.querySelector("#result");
+
+submitButton.addEventListener('click', () => {
+    result.innerHTML = "clicked";
+})
