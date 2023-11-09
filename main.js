e48aa01b49faf1b122fa39c173f497a4896c1a90
FayzaanV <fayzaanvirk6@yahoo.com>
2023-11-09 15:52:39 -0600

    11/9

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ let palindromeInput = document.querySelector("#palindromeInput");
 let submitButton = document.querySelector("#submitButton");
 let result = document.querySelector("#result");
 
+let submittedText = palindromeInput.value;
+let isPalindrome = true;
+
 submitButton.addEventListener('click', () => {
-    result.innerHTML = "clicked";
+    for (let i = 0; i < submittedText.length / 2; i++) {
+        if (submittedText[i].toLowerCase() != (submittedText[submittedText.length - (i + 1)].toLowerCase())) {
+            isPalindrome = false;
+        }
+    }
+
+    if (isPalindrome == true) {
+        result.innerHTML = "This is a palindrome";
+    } else if (isPalindrome == false) {
+        result.innerHTML = "This is not a palindrome";
+    }
 })
